e2ac1396bad2e864a5f0fc5f7da360386422c2df
Amit Patel <amitpatel@Amits-MacBook-Air.local>
2022-09-04 11:35:38 +0530

    Added Support for UI

--- compute_edges.py
+++ compute_edges.py
@@ -29,3 +29,36 @@ def compute_deriv(image = None):
     final[final < threshold] = 0
 
     return final
+
+import streamlit as st
+from PIL import Image, ImageOps
+
+def st_ui():
+    '''
+    Function running the Streamlit UI.
+    Doesn't return anything.
+    '''
+    
+    st.set_page_config(layout = "wide")
+    st.title("Compute edges")
+
+    user_image = st.sidebar.file_uploader("Load your own image")
+    if user_image is not None:
+        im = Image.open(user_image)
+        imagegray = ImageOps.grayscale(im)
+        image = np.array(imagegray).astype(np.float32)
+    
+    else:
+        im = scipy.misc.face()
+        image = misc.face(gray=True).astype(np.float32)
+
+    st.header("Original image")
+    st.image(im)
+
+    final = compute_deriv(image)
+    
+    st.header("Edge image")
+    st.image(final)
+
+if __name__ == "__main__":
+    st_ui()
